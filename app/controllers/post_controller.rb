57cd0926006240b5848ab710a0b8d2547d4431fc
Mallivance <ashpek01@gmail.com>
2020-05-31 23:24:25 +0300

    Implement create post validation

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -7,6 +7,10 @@ class PostController < ApplicationController
   end
 
   def add
+    unless validate_params
+      return
+    end
+
     user_id = session[:user_id]
     unless user_id
       redirect_to login_login_path
@@ -16,6 +20,18 @@ class PostController < ApplicationController
     @post = Post.create(post_params)
 
     redirect_to('/feed/feed')
+  end
+
+  def validate_params
+    unless params[:image]
+      show_error "Please select image"
+      return false
+    end
+    true
+  end
+
+  def show_error(message)
+    redirect_to post_post_path, notice: message
   end
 
   def post_params
